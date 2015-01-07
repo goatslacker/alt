8af1ccdc95c43cb0d2fc0092f5f44a72802e527f
Josh Perez <josh@goatslacker.com>
2015-01-06 20:18:47 -0800

    Throw if no altStores prop

--- IsomorphicMixin.js
+++ IsomorphicMixin.js
@@ -3,6 +3,11 @@ module.exports = { create: createIsomorphicMixin }
 function createIsomorphicMixin(alt) {
   return {
     componentWillMount: function () {
+      if (!this.props.altStores) {
+        throw new ReferenceError(
+          'altStores was not provided as a property to the react element'
+        )
+      }
       alt.bootstrap(JSON.stringify(this.props.altStores))
     }
   }
