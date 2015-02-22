6696bb0085f037532064140917c19274a12be89a
Josh Perez <josh@goatslacker.com>
2015-02-22 01:12:02 -0800

    Move ReactComponent to a helper

--- test/index.js
+++ test/index.js
@@ -10,50 +10,7 @@ let assert = require('assert')
 
 let alt = new Alt()
 
-class ReactComponent {
-  setState(state) {
-    this.state = state
-    this.render()
-  }
-
-  // A not at all react spec compliant way to test fake react components
-  static test(Component, testFn, props) {
-    let component = new Component()
-
-    Component.mixins.forEach((mixin) => {
-      // transfer over the mixins.
-      Object.keys(mixin).forEach((method) => {
-        component[method] = mixin[method].bind(component)
-      })
-
-      // move over the statics
-      if (Component.statics) {
-        Object.keys(Component.statics).forEach((stat) => {
-          Component[stat] = Component.statics[stat]
-        })
-      }
-    })
-
-    try {
-      component.props = component.getDefaultProps
-        ? component.getDefaultProps()
-        : {}
-
-      component.state = component.getInitialState
-        ? component.getInitialState()
-        : {}
-
-      component.componentWillMount && component.componentWillMount();
-      component.componentDidMount && component.componentDidMount();
-
-      testFn && testFn();
-    } catch (e) {
-      throw e;
-    } finally {
-      component.componentWillUnmount && component.componentWillUnmount();
-    }
-  }
-}
+import ReactComponent from './helpers/ReactComponent'
 
 class MyActions {
   constructor() {
