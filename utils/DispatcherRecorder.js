6b8b6682f233d9ad1d93cdcaf92643d2df41c497
Josh Perez <josh@goatslacker.com>
2015-02-25 23:33:15 -0800

    Add some inline documentation

--- utils/DispatcherRecorder.js
+++ utils/DispatcherRecorder.js
@@ -1,3 +1,30 @@
+/**
+ * DispatcherRecorder(alt: AltInstance): DispatcherInstance
+ *
+ * > Record and replay your actions at any point in time.
+ *
+ * This util allows you to record a set of dispatches which you can later
+ * replay at your convenience.
+ *
+ * Good for: Debugging, repeating, logging.
+ *
+ * Usage:
+ *
+ * ```js
+ * var recorder = new DispatcherRecorder(alt);
+ *
+ * // start recording
+ * recorder.record();
+ *
+ * // call a series of actions
+ *
+ * // stop recording
+ * recorder.stop();
+ *
+ * // replay the events that took place
+ * recorder.replay();
+ * ```
+ */
 module.exports = DispatcherRecorder
 
 function DispatcherRecorder(alt) {
@@ -6,6 +33,11 @@ function DispatcherRecorder(alt) {
   this.dispatchToken = null
 }
 
+/**
+ * record(): boolean
+ * If recording started you get true, otherwise false since there's a recording
+ * in progress.
+ */
 DispatcherRecorder.prototype.record = function () {
   if (this.dispatchToken) {
     return false
@@ -18,15 +50,24 @@ DispatcherRecorder.prototype.record = function () {
   return true
 }
 
+/**
+ * Stops the recording in progress.
+ */
 DispatcherRecorder.prototype.stop = function () {
   this.alt.dispatcher.unregister(this.dispatchToken)
   this.dispatchToken = null
 }
 
+/**
+ * Clear all events from memory.
+ */
 DispatcherRecorder.prototype.clear = function () {
   this.events = []
 }
 
+/**
+ * Synchronously replay all events that were recorded.
+ */
 DispatcherRecorder.prototype.replay = function () {
   this.events.forEach(function (payload) {
     this.alt.dispatch(payload.action, payload.data)
--- utils/makeFinalStore.js
+++ utils/makeFinalStore.js
@@ -1,3 +1,26 @@
+/**
+ * makeFinalStore(alt: AltInstance): AltStore
+ *
+ * > Creates a `FinalStore` which is a store like any other except that it
+ * waits for all other stores in your alt instance to emit a change before it
+ * emits a change itself.
+ *
+ * Want to know when a particular dispatch has completed? This is the util
+ * you want.
+ *
+ * Good for: taking a snapshot and persisting it somewhere, saving data from
+ * a set of stores, syncing data, etc.
+ *
+ * Usage:
+ *
+ * ```js
+ * var FinalStore = makeFinalStore(alt);
+ *
+ * FinalStore.listen(function () {
+ *   // all stores have now changed
+ * });
+ * ```
+ */
 module.exports = makeFinalStore
 
 function FinalStore() {
