ff9e4dd035b13f93fcb4a4afa372edefaa33d71b
Josh Perez <josh@goatslacker.com>
2015-04-17 09:11:19 -0700

    Allow customizing your state property key

--- dist/alt-browser.js
+++ dist/alt-browser.js
@@ -1201,7 +1201,7 @@ function createStoreFromClass(alt, StoreModel, key) {
 
   var store = _applyConstructor(Store, argsForConstructor);
 
-  storeInstance = assign(new AltStore(alt.dispatcher, store, null, StoreModel), getInternalMethods(StoreModel, builtIns));
+  storeInstance = assign(new AltStore(alt.dispatcher, store, typeof alt._stateKey === "string" ? store[alt._stateKey] : null, StoreModel), getInternalMethods(StoreModel, builtIns));
 
   return storeInstance;
 }
@@ -1218,6 +1218,7 @@ var Alt = (function () {
     this.actions = {};
     this.stores = {};
     this[LAST_SNAPSHOT] = this[INIT_SNAPSHOT] = "{}";
+    this._stateKey = config.stateKey;
   }
 
   _createClass(Alt, {
