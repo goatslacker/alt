6cf9d3792eb82f2e1747703864080ac0ab50b008
Josh Perez <josh@goatslacker.com>
2015-04-17 15:17:09 -0700

    Remove symbol descriptors

--- src/shared/symbols.js
+++ src/shared/symbols.js
@@ -1,15 +1,37 @@
-'use strict'
 import Symbol from 'es-symbol'
 
-export const ACTION_HANDLER = Symbol('action creator handler')
-export const ACTION_KEY = Symbol('holds the actions uid symbol for listening')
-export const ACTION_UID = Symbol('the actions uid name')
-export const ALL_LISTENERS = Symbol('name of listeners')
-export const EE = Symbol('event emitter instance')
-export const INIT_SNAPSHOT = Symbol('init snapshot storage')
-export const LAST_SNAPSHOT = Symbol('last snapshot storage')
-export const LIFECYCLE = Symbol('store lifecycle listeners')
-export const LISTENERS = Symbol('stores action listeners storage')
-export const PUBLIC_METHODS = Symbol('store public method storage')
+// action creator handler
+export const ACTION_HANDLER = Symbol()
+
+// the action's uid symbol for listening
+export const ACTION_KEY = Symbol()
+
+// the action's name
+export const ACTION_UID = Symbol()
+
+// store all of a store's listeners
+export const ALL_LISTENERS = Symbol()
+
+// event emitter instance
+export const EE = Symbol()
+
+// initial snapshot
+export const INIT_SNAPSHOT = Symbol()
+
+// last snapshot
+export const LAST_SNAPSHOT = Symbol()
+
+// all lifecycle listeners
+export const LIFECYCLE = Symbol()
+
+// store action listeners
+export const LISTENERS = Symbol()
+
+// public methods
+export const PUBLIC_METHODS = Symbol()
+
+// boolean if state has changed for emitting change event
 export const STATE_CHANGED = Symbol()
-export const STATE_CONTAINER = Symbol('the state container')
+
+// contains all state
+export const STATE_CONTAINER = Symbol()
