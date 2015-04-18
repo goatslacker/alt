fc381c8392b4066ce460505fb17c62eea8677370
Josh Perez <josh@goatslacker.com>
2015-04-17 19:56:07 -0700

    Rename to AltAction

--- src/alt/AltAction.js
+++ src/alt/AltAction.js
@@ -1,6 +1,6 @@
 import { ACTION_HANDLER, ACTION_UID } from './symbols/symbols'
 
-export default class ActionCreator {
+export default class AltAction {
   constructor(alt, name, action, actions) {
     this[ACTION_UID] = name
     this[ACTION_HANDLER] = action.bind(this)
