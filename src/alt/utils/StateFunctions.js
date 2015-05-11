f0ad628eca69f73fa63320bd5d3d70104e09de86
Josh Perez <josh@goatslacker.com>
2015-05-10 23:10:59 -0700

    push state

--- src/alt/utils/StateFunctions.js
+++ src/alt/utils/StateFunctions.js
@@ -22,7 +22,7 @@ export function snapshot(instance, storeNames = []) {
     const storeName = storeHandle.displayName || storeHandle
     const store = instance.stores[storeName]
     const { config } = store.StoreModel
-    store[Sym.LIFECYCLE].snapshot.emit()
+    store[Sym.LIFECYCLE].snapshot.push()
     const customSnapshot = config.onSerialize &&
       config.onSerialize(store[Sym.STATE_CONTAINER])
     obj[storeName] = customSnapshot ? customSnapshot : store.getState()
