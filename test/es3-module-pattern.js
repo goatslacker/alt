c4286babd543b7d1ab5fa0b12e105f0740237c9f
Josh Perez <josh@goatslacker.com>
2015-05-10 23:14:26 -0700

    fix all unlisten tests

--- test/es3-module-pattern.js
+++ test/es3-module-pattern.js
@@ -45,7 +45,7 @@ export default {
 
     'store method exists'() {
       const storePrototype = Object.getPrototypeOf(myStore)
-      const assertMethods = ['constructor', 'emitChange', 'listen', 'unlisten', 'getState']
+      const assertMethods = ['constructor', 'emitChange', 'listen', 'getState']
       assert.deepEqual(Object.getOwnPropertyNames(storePrototype), assertMethods, 'methods exist for store')
       assert.isUndefined(myStore.addListener, 'event emitter methods not present')
       assert.isUndefined(myStore.removeListener, 'event emitter methods not present')
