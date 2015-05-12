1fe5f032370536a6a8dbde32bf1946523d130c4f
Josh Perez <josh@goatslacker.com>
2015-05-12 10:51:57 -0700

    Removing symbols

--- src/alt/actions/index.js
+++ src/alt/actions/index.js
@@ -1,6 +1,6 @@
 import Symbol from 'es-symbol'
 
-import * as Sym from '../symbols/symbols'
+import * as fn from '../../utils/functions'
 import * as utils from '../utils/AltUtils'
 
 class AltAction {
