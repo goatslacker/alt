a8424a2f53149558db4e94456a6e474e4bfe3dbc
Jaroslaw Zabiello <hipertracker@gmail.com>
2015-06-25 03:00:09 +0100

    replace  browserify with webpack for bulding dist/

---
+++ dist.min.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    context: __dirname + '/src',
+    entry: {
+        'alt': ['./alt/index.js'],
+        'alt-with-addons': ['./alt/addons.js']
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].min.js',
+        library: 'Alt',
+        libraryTarget: 'umd'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel',
+            exclude: /node_modules/
+        }]
+    }
+};
