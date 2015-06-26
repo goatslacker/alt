d5adc46e91453629cebf2fd66de54b4a1fbada94
Josh Perez <josh@goatslacker.com>
2015-06-26 13:27:02 -0700

    Add failing AltIso test

--- test/alt-iso-test.js
+++ test/alt-iso-test.js
@@ -137,14 +137,42 @@ export default {
           assert.match(values[0].html, /AAAAAA/)
           assert.match(values[0].html, /111111/)
 
+          assert.notMatch(values[0].html, /BBBBBB/)
+          assert.notMatch(values[0].html, /222222/)
+          assert.notMatch(values[0].html, /CCCCCC/)
+          assert.notMatch(values[0].html, /333333/)
+          assert.notMatch(values[0].html, /DDDDDD/)
+          assert.notMatch(values[0].html, /444444/)
+
           assert.match(values[1].html, /BBBBBB/)
           assert.match(values[1].html, /222222/)
+
+          assert.notMatch(values[1].html, /AAAAAA/)
+          assert.notMatch(values[1].html, /111111/)
+          assert.notMatch(values[1].html, /CCCCCC/)
+          assert.notMatch(values[1].html, /333333/)
+          assert.notMatch(values[1].html, /DDDDDD/)
+          assert.notMatch(values[1].html, /444444/)
 
           assert.match(values[2].html, /CCCCCC/)
           assert.match(values[2].html, /333333/)
 
+          assert.notMatch(values[2].html, /AAAAAA/)
+          assert.notMatch(values[2].html, /111111/)
+          assert.notMatch(values[2].html, /BBBBBB/)
+          assert.notMatch(values[2].html, /222222/)
+          assert.notMatch(values[2].html, /DDDDDD/)
+          assert.notMatch(values[2].html, /444444/)
+
           assert.match(values[3].html, /DDDDDD/)
           assert.match(values[3].html, /444444/)
+
+          assert.notMatch(values[3].html, /AAAAAA/)
+          assert.notMatch(values[3].html, /111111/)
+          assert.notMatch(values[3].html, /BBBBBB/)
+          assert.notMatch(values[3].html, /222222/)
+          assert.notMatch(values[3].html, /CCCCCC/)
+          assert.notMatch(values[3].html, /333333/)
 
           done()
         })
