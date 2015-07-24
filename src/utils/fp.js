2b5adb360e8113d38a879f92c75aeb37f863a7c9
Josh Perez <josh@goatslacker.com>
2015-07-24 14:58:21 -0700

    Make fp tools faster by pulling directly from state

--- src/utils/fp.js
+++ src/utils/fp.js
@@ -4,19 +4,19 @@ const { push } = Array.prototype
 /*eslint-disable no-shadow*/
 export function map(fn, stores) {
   return stores
-    ? stores.map(store => fn(store.getState()))
+    ? stores.map(store => fn(store.state))
     : stores => map(fn, stores)
 }
 
 export function filter(fn, stores) {
   return stores
-    ? stores.filter(store => fn(store.getState()))
+    ? stores.filter(store => fn(store.state))
     : stores => filter(fn, stores)
 }
 
 export function reduce(fn, stores, acc = {}) {
   return stores
-    ? stores.reduce((acc, store) => fn(acc, store.getState()), acc)
+    ? stores.reduce((acc, store) => fn(acc, store.state), acc)
     : stores => reduce(fn, stores)
 }
 
@@ -24,7 +24,7 @@ export function flatMap(fn, stores) {
   if (!stores) return (stores) => flatMap(fn, stores)
 
   return stores.reduce((result, store) => {
-    let value = fn(store.getState())
+    let value = fn(store.state)
     if (Array.isArray(value)) {
       push.apply(result, value)
     } else {
@@ -42,7 +42,7 @@ export function zipWith(fn, a, b) {
   const length = Math.min(a.length, b.length)
   const result = Array(length)
   for (let i = 0; i < length; i += 1) {
-    result[i] = fn(a[i].getState(), b[i].getState())
+    result[i] = fn(a[i].state, b[i].state)
   }
   return result
 }
