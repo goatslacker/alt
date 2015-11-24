9b2b4ef158449693dff64cb1561ce0aea1dbba28
Josh Perez <josh@goatslacker.com>
2015-11-23 21:37:51 -0800

    Build alt

--- dist/alt.js
+++ dist/alt.js
@@ -1326,12 +1326,18 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 10 */
 /***/ function(module, exports) {
 
-	"use strict";
+	'use strict';
 
 	function transmitter() {
 	  var subscriptions = [];
+	  var pushing = false;
+	  var toUnsubscribe = [];
 
 	  var unsubscribe = function unsubscribe(onChange) {
+	    if (pushing) {
+	      toUnsubscribe.push(onChange);
+	      return;
+	    }
 	    var id = subscriptions.indexOf(onChange);
 	    if (id >= 0) subscriptions.splice(id, 1);
 	  };
@@ -1345,15 +1351,24 @@ return /******/ (function(modules) { // webpackBootstrap
 	  };
 
 	  var push = function push(value) {
-	    subscriptions.forEach(function (subscription) {
-	      return subscription(value);
-	    });
+	    if (pushing) throw new Error('Cannot push while pushing');
+	    pushing = true;
+	    try {
+	      subscriptions.forEach(function (subscription) {
+	        return subscription(value);
+	      });
+	    } finally {
+	      pushing = false;
+	      toUnsubscribe = toUnsubscribe.filter(unsubscribe);
+	    }
 	  };
 
-	  return { subscribe: subscribe, push: push, unsubscribe: unsubscribe };
+	  return { subscribe: subscribe, push: push, unsubscribe: unsubscribe, subscriptions: subscriptions };
 	}
 
 	module.exports = transmitter;
+
+
 
 /***/ },
 /* 11 */
