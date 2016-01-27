d276f54a31e75adef148ca3e81f540cbc88dda9a
Josh Perez <josh@goatslacker.com>
2016-01-26 16:45:36 -0800

    Revert "Removes data sources"

--- src/store/StoreMixin.js
+++ src/store/StoreMixin.js
@@ -19,6 +19,74 @@ const StoreMixin = {
     this.dispatcher.waitFor(tokens)
   },
 
+  exportAsync(asyncMethods) {
+    this.registerAsync(asyncMethods)
+  },
+
+  registerAsync(asyncDef) {
+    let loadCounter = 0
+
+    const asyncMethods = fn.isFunction(asyncDef)
+      ? asyncDef(this.alt)
+      : asyncDef
+
+    const toExport = Object.keys(asyncMethods).reduce((publicMethods, methodName) => {
+      const desc = asyncMethods[methodName]
+      const spec = fn.isFunction(desc) ? desc(this) : desc
+
+      const validHandlers = ['success', 'error', 'loading']
+      validHandlers.forEach((handler) => {
+        if (spec[handler] && !spec[handler].id) {
+          throw new Error(`${handler} handler must be an action function`)
+        }
+      })
+
+      publicMethods[methodName] = (...args) => {
+        const state = this.getInstance().getState()
+        const value = spec.local && spec.local(state, ...args)
+        const shouldFetch = spec.shouldFetch
+          ? spec.shouldFetch(state, ...args)
+          /*eslint-disable*/
+          : value == null
+          /*eslint-enable*/
+        const intercept = spec.interceptResponse || (x => x)
+
+        const makeActionHandler = (action, isError) => {
+          return (x) => {
+            const fire = () => {
+              loadCounter -= 1
+              action(intercept(x, action, args))
+              if (isError) throw x
+            }
+            return this.alt.trapAsync ? () => fire() : fire()
+          }
+        }
+
+        // if we don't have it in cache then fetch it
+        if (shouldFetch) {
+          loadCounter += 1
+          /* istanbul ignore else */
+          if (spec.loading) spec.loading(intercept(null, spec.loading, args))
+          return spec.remote(state, ...args).then(
+            makeActionHandler(spec.success),
+            makeActionHandler(spec.error, 1)
+          )
+        }
+
+        // otherwise emit the change now
+        this.emitChange()
+        return value
+      }
+
+      return publicMethods
+    }, {})
+
+    this.exportPublicMethods(toExport)
+    this.exportPublicMethods({
+      isLoading: () => loadCounter > 0,
+    })
+  },
+
   exportPublicMethods(methods) {
     fn.eachObject((methodName, value) => {
       if (!fn.isFunction(value)) {
