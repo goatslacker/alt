ea92aca4f0d4c62b66ad997ebcf830a8f22fda17
Ryan <ryan.king@epicgames.com>
2017-11-10 14:30:47 -0500

    Fix a few missed files

--- web/scripts/search.js
+++ web/scripts/search.js
@@ -1,14 +1,14 @@
-import SearchApp from 'alt-search-docs'; //eslint-disable-line
-import React from 'react'; //eslint-disable-line
-import ReactDOM from 'react-dom'; //eslint-disable-line
+import SearchApp from 'alt-search-docs' //eslint-disable-line
+import React from 'react' //eslint-disable-line
+import ReactDOM from 'react-dom' //eslint-disable-line
 
-const github = /goatslacker.github.io\/alt/;
+const github = /goatslacker.github.io\/alt/
 
 if (github.test(location.href)) {
-  location.href = location.href.replace(github, 'alt.js.org');
+  location.href = location.href.replace(github, 'alt.js.org')
 }
 
 ReactDOM.render(
   <SearchApp url="/assets/search.json" />,
   document.getElementById('alt-search-app')
-);
+)
